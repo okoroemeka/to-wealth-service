d42e8c420640c2af97e68e5a6c76b07033ec913d
Emeka Okoro <emeka@Emekas-MacBook-Pro.local>
2020-09-17 22:43:36 +0100

    remove express session

--- src/middlewares/index.js
+++ src/middlewares/index.js
@@ -1,7 +1,6 @@
 import bodyParser from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
-import expressSession from 'express-session';
 import dotenv from 'dotenv';
 
 dotenv.config();
@@ -10,12 +9,5 @@ module.exports = (app) => {
   app.use(cors());
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: false }));
-  app.use(
-    expressSession({
-      secret: process.env.SESSION_SECRET,
-      resave: false,
-      saveUninitialized: false,
-    })
-  );
   app.use(morgan('dev'));
 };
